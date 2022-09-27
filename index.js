25554d3de74f947e40c96ec24b4ab26bd9423f51
Said-Guzman <112099452+Said-Guzman@users.noreply.github.com>
2022-09-27 00:03:17 -0400

    finished Javascript

--- index.js
+++ index.js
@@ -8,17 +8,55 @@ function min (a,b){
 console.log(min)
 
 
-const fName = ["seth" , "said", "tests"];
+const fName = ["seth " , "said ", "tests "];
 const lName = ["the best" , "the greatest" , "are bad" ];
 const age = ["19", "20" , "21"];
 
-
-
-console.log("Hello "+ "Name "+ "Is " [fName1] + "")
-
+console.log("Hello My Name Is " + fName[1] + "" + lName[0] + " and im " + age[2] + " years old" )
 
 
 
+let num = prompt("Enter a Number of a Month:");
+switch (num){
+    case '1':
+        alert('January');
+break;
+case '2':
+        alert('Febuary');
+break;
+case '3':
+        alert('March');
+break;
+case '4':
+        alert('April');
+break;
+case '5':
+        alert('May');
+break;
+case '6':
+        alert('June');
+break;
+case '7':
+        alert('July');
+break;
+case '8':
+        alert('August');
+break;
+case '9':
+        alert('September');
+break;
+case '10':
+        alert('October');
+break;
+case '11':
+        alert('November');
+break;
+case '12':
+        alert('December');
+break;
+default:
+    alert('its just 1 through 12 bro...');
+}
 
 
 
