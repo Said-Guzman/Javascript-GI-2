ceb420661b1e452c00488d4dff03bf409c853018
Said-Guzman <112099452+Said-Guzman@users.noreply.github.com>
2022-09-26 22:31:39 -0400

    javascript

---
+++ index.js
@@ -0,0 +1,78 @@
+var b = 2
+var a = 5
+min = (a- b)
+function min (a,b){
+    (a - b);
+}
+
+console.log(min)
+
+
+const fName = ["seth" , "said", "tests"];
+const lName = ["the best" , "the greatest" , "are bad" ];
+const age = ["19", "20" , "21"];
+
+
+
+console.log("Hello "+ "Name "+ "Is " [fName1] + "")
+
+
+
+
+
+
+
+
+
+
+
+
+var mM = .008
+var mH = .228
+
+z = mM / (mH*mH)
+console.log(z)
+
+var jM = .045
+var jH = .254
+
+x= jM / (jH*jH)
+console.log(x)
+
+if (z<x){
+    console.log(true)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
